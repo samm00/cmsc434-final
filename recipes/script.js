0de31270c23b1f1265fe68963771ea2c3cfdc29d
Kaitlyn019 <kaitlynyyang@gmail.com>
2022-11-02 20:04:10 -0400

    recipes updates

--- recipes/script.js
+++ recipes/script.js
@@ -12,6 +12,23 @@ function todoChecked(id){
     }
 }
 
+function addToLocalStorage(id){
+    console.log(id)
+    var elem = document.getElementById(id+"-text")
+    var btn = document.getElementById(id+"-plus")
+
+    if (!btn.classList.contains('added-to-list')){
+        if (window.localStorage.getItem('AddToList') == null || window.localStorage.getItem('AddToList') == "") {
+            window.localStorage.setItem('AddToList', elem.innerText);
+        } else {
+            window.localStorage.setItem('AddToList', window.localStorage['AddToList'] + "," + elem.innerText);
+        }
+    
+        btn.innerText = '\u{2713}';
+        btn.classList.add('added-to-list');
+    } 
+}
+
 function crossOut(id){
     var elem = document.getElementById(id);
     if (elem.innerHTML.includes("<del>")){
