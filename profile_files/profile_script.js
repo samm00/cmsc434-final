6f3bb4540b1e3ddb16df1aabeefc1da05f286fc0
jzhang1717 <113122456+jzhang1717@users.noreply.github.com>
2022-11-03 23:15:02 -0400

    Update to Profile Page
    Final-ish changes to profile page, all major functions implemented (I think). Styling follows stylesheet, can be changed easily.

--- profile_files/profile_script.js
+++ profile_files/profile_script.js
@@ -2,9 +2,10 @@
 //               {name:"Kandy Kane", restrict:"Peanut Allergy, Lactose Intolerant", pref:"Sweets"},
 //               {name:"Tom A. Tow", restrict:"None", pref:"None"}]
 
+
 //--------------------- functions to change menus ---------------------
 function openHousehold(){
-    document.getElementById("title").innerHTML = "Household";
+    document.getElementById("header").innerHTML = "<h1 id=\"backButton\" onclick=\"returnToMenu()\">❮</h1><h1 id=\"title\">Household</h1>";
     document.getElementById("bottomhalf").style.display = "none";
     document.getElementById("tophalf").style.display = "none";
     document.getElementById("household").style.display = "block";
@@ -12,36 +13,41 @@ function openHousehold(){
 }
 
 function openSettings(){
-    document.getElementById("title").innerHTML = "Settings";
+    document.getElementById("header").innerHTML = "<h1 id=\"backButton\" onclick=\"returnToMenu()\">❮</h1><h1 id=\"title\">Settings</h1>";
     document.getElementById("bottomhalf").style.display = "none";
     document.getElementById("tophalf").style.display = "none";
     document.getElementById("settings").style.display = "block";
 }
 
 function openChangeAccounts(){
+    document.getElementById("header").innerHTML = "<h1 id=\"backButton\" onclick=\"returnToMenu()\">❮</h1><h1 id=\"title\">Change Accounts</h1>";
     document.getElementById("bottomhalf").style.display = "none";
+    document.getElementById("tophalf").style.borderBottom = "none";
     document.getElementById("change").style.display = "block";
-    document.getElementById("changebuttons").style.display = "block";
 }
 
 function returnToMenu(){
-    document.getElementById("title").innerHTML = "Profile";
+document.getElementById("header").innerHTML = "<h1 id=\"title\">Profile</h1>";
+    document.getElementById("tophalf").style.borderBottom = "#000 solid 2px";
     document.getElementById("tophalf").style.display = "block";
     document.getElementById("bottomhalf").style.display = "block";
     document.getElementById("change").style.display = "none";
     document.getElementById("settings").style.display = "none";
     document.getElementById("household").style.display = "none";
     document.getElementById("housebuttons").style.display = "none";
-    document.getElementById("changebuttons").style.display = "none";
+    document.getElementById("addMenu").style.display = "none";
+    document.getElementById("unimple").style.display = "none";
 }
 
 function openAdd(){
+    document.getElementById("header").innerHTML = "<h1 id=\"title\">Household</h1>";
     document.getElementById("household").style.display = "none";
     document.getElementById("housebuttons").style.display = "none";
     document.getElementById("addMenu").style.display = "block";
 }
 
 function closeAdd(){
+    document.getElementById("header").innerHTML = "<h1 id=\"backButton\" onclick=\"returnToMenu()\">❮</h1><h1 id=\"title\">Household</h1>";
     document.getElementById("addMenu").style.display = "none";
     document.getElementById("household").style.display = "block";
     document.getElementById("housebuttons").style.display = "block";
@@ -75,10 +81,16 @@ function addMember(){
     houseCode += "<h2 class=\"member_name\">"+ newName +"</h2> ";
     houseCode += "<p class=\"member_desc\">Allergies/Dietary Restrictions: " + newR + "</p> ";
     houseCode += "<p class=\"member_desc\">Preferences: " + newPref + "</p> ";
+    houseCode += "<div id=\"" + targetID + "Delete\">";
+    houseCode += "<button class=\"deleteMem\" onclick=\"openDeleteConfirm('" + targetID + "Delete', '" + targetID + "Confirm')\">Delete</button> </div>";
+    houseCode += "<div id=\"" + targetID + "Confirm\" style=\"display: none;\">";
+    houseCode += "<p>Are you sure you want to delete this user?</p>";
+    houseCode += "<button class=\"button\" onclick=\"closeDeleteConfirm('" + targetID + "Delete', '" + targetID + "Confirm')\">Cancel</button>    ";
     houseCode += "<button class=\"deleteMem\" onclick=\"deleteMember('" + targetID + "', '" + newName + "')\">Delete</button> </div>";
+
     document.getElementById("household").innerHTML += houseCode;
 
-    accountCode += "<button class=\"buttons " + targetID + "\" onclick=\"changeAccount('" + newName + "')\">" + newName + "</button>";
+    accountCode += "<button class=\"menuButton " + targetID + "\" onclick=\"changeAccount('" + newName + "')\">" + newName + "</button>";
     document.getElementById("change").innerHTML += accountCode;
     
     document.getElementById("newName").value = "";
@@ -90,17 +102,33 @@ function addMember(){
 //delete member and everything associated
 function deleteMember(targets, name){
     let toDelete = document.getElementsByClassName(targets);
-
     while(toDelete[0]) {
         toDelete[0].remove();//.parentNode.removeChild(toDelete[0]);
     }
-
+    
     if(document.getElementById("name").textContent == name){
         document.getElementById("name").innerHTML = "No Account";
     }
 }
 
+//open confirmation message when deleting member
+function openDeleteConfirm(deleteID, confirmID){
+    document.getElementById(deleteID).style.display = "none";
+    document.getElementById(confirmID).style.display = "block";
+}
+
+//close confirmation message when canceling delete member
+function closeDeleteConfirm(deleteID, confirmID){
+    document.getElementById(deleteID).style.display = "block";
+    document.getElementById(confirmID).style.display = "none";
+}
+
 //simple change account
 function changeAccount(name){
     document.getElementById("name").innerHTML = name;
+    returnToMenu();
+}
+
+function toggleUnimple(){
+    document.getElementById("unimple").style.display = "inline";
 }
